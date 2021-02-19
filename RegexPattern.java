21724b9903ec887964ea47fcc687fcb9bbbece90
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-19 18:20:23 +0530

    Pincode and Email Verification Patterns Regex Programs

---
+++ RegexPattern.java
@@ -0,0 +1,7 @@
+public class RegexPattern
+{
+	public static void main(String args[])
+	{
+		System.out.println("\t\t Welcome to Regex in Java \nUse Case of Pincode and Email Patterns Verifications");
+	}
+}
