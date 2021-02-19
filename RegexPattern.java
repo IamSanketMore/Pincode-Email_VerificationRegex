501956d7692a9cc665053c127d6ee299982dec2c
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-19 18:25:28 +0530

    PinCodeUC1- Check pincode is getting exact 6 numbers

--- RegexPattern.java
+++ RegexPattern.java
@@ -1,7 +1,37 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class RegexPattern
 {
-	public static void main(String args[])
+
+	public void checkPinCode(String pincode)
 	{
-		System.out.println("\t\t Welcome to Regex in Java \nUse Case of Pincode and Email Patterns Verifications");
+		String regex = "[0-9]{6}";
+
+		Pattern patternChecker = Pattern.compile(regex);
+		Matcher matchChecker = patternChecker.matcher(pincode);
+
+		if(matchChecker.matches())
+		{
+			System.out.println("Valid PinCode");
+		}
+		else
+		{
+			System.out.println("InValid PinCode");
+		}
+		
+	}
+	public static void main(String args [])
+	{
+		Scanner scanReader = new Scanner(System.in);
+
+		RegexPattern code = new RegexPattern();
+
+		System.out.println("Enter PinCode:-");
+		String pinCode = scanReader.nextLine();
+		code.checkPinCode(pinCode);
+		
+		scanReader.close();
 	}
 }
